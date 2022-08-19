114e25eb82e09c08eb7bf7cb34e3895bbb673f9b
Mordinii <allan.mordini@hotmail.com>
2022-08-19 14:13:57 -0300

    Implementando o roteamento de acessos do menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { rootRouterConfig } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    ContatoComponent,
+    SobreComponent  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash:false })]
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/"}  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
